7bc27e5642eeda7c832a9bac6b3d5e4f9ae01cf8
Shabana <shabanashajahan052003@gmail.com>
2026-04-14 16:20:13 +0530

    Initial commit - Weather App

---
+++ weather/views.py
@@ -0,0 +1,45 @@
+import requests
+from django.shortcuts import render
+
+def home(request):
+    city = request.GET.get('city')
+
+    if city:
+        api_key = "YOUR_API_KEY"   
+        # CURRENT WEATHER
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        res = requests.get(url).json()
+
+        if str(res.get("cod")) != "200":
+            return render(request, 'index.html', {
+                'error': "City not found"
+            })
+
+        # FORECAST (5 entries)
+        forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+        forecast_res = requests.get(forecast_url).json()
+
+        forecast_data = []
+        for i in forecast_res['list'][0:5]:
+            forecast_data.append({
+                'temp': i['main']['temp'],
+                'icon': i['weather'][0]['icon'],
+                'time': i['dt_txt']
+            })
+
+        data = {
+            'city': city,
+            'temperature': res['main']['temp'],
+            'description': res['weather'][0]['description'],
+            'humidity': res['main']['humidity'],
+            'wind': res['wind']['speed'],
+            'feels': res['main']['feels_like'],
+            'icon': res['weather'][0]['icon']
+        }
+
+        return render(request, 'index.html', {
+            'data': data,
+            'forecast': forecast_data
+        })
+
+    return render(request, 'index.html')
